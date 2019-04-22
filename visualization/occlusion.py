ad119409fa27e0ef31b43428a8362c4f92f8f62e
Mikołaj Walczak <mikiwalczak+github@gmail.com>
2019-04-22 22:11:21 +0200

    Visualization - occlusion

---
+++ visualization/occlusion.py
@@ -0,0 +1,43 @@
+import click
+
+import torch
+import torch.nn.functional as F
+
+import matplotlib.pyplot as plt
+
+
+def _plot_image(img, title):
+    plt.imshow(img)
+    plt.title(title)
+    plt.axis('off')
+
+
+def occlustion(net, image, label, k=10, stride=4):
+    _, C, W, H = image.shape
+    width = int((W - k) / stride + 1)
+    height = int((H - k) / stride + 1)
+    heatmap = torch.zeros(width, height)
+    image = image.data
+
+    with click.progressbar(range(0, H - k + 1, stride)) as bar:
+        for y, i in enumerate(bar):
+            for x, j in enumerate(range(0, W - k + 1, stride)):
+                tmp = image.clone()
+                tmp[:, :, j:j + k, i:i + k] = 0
+                softmax = F.softmax(net(tmp), dim=1).data[0]
+                heatmap[y, x] = softmax[label]
+
+    image = image.squeeze()
+
+    fig = plt.figure()
+    plt.rcParams["figure.figsize"] = (20, 20)
+
+    fig.add_subplot(1, 2, 1)
+    _plot_image(image.transpose(0, 1).transpose(1, 2), 'Original')
+
+    fig.add_subplot(1, 2, 2)
+    _plot_image(heatmap, 'Heatmap')
+
+    plt.show()
+
+    return heatmap
